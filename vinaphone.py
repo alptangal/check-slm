6225f8bad90eddaaebd677e8455d3a8a8cc84450
phuongdomegamarket <phuongdomegamarket@gmail.com>
2024-05-25 13:04:09 +0700

    Update vinaphone.py

--- vinaphone.py
+++ vinaphone.py
@@ -1,4 +1,4 @@
-import requests
+import httpx
 from bs4 import BeautifulSoup as Bs4
 import time
 import os
@@ -7,15 +7,14 @@ import random,json
 from urllib.parse import unquote
 import aiohttp
 import urllib3,re
-requests.packages.urllib3.disable_warnings()
-requests.packages.urllib3.util.ssl_.DEFAULT_CIPHERS += 'HIGH:!DH:!aNULL'
+httpx._config.DEFAULT_CIPHERS += ':HIGH:!DH:!aNULL'
 async def sendOtp(phone):
   url='https://my.vnpt.com.vn/mapi/services/otp_send'
   headers={
     'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:122.0) Gecko/20100101 Firefox/122.0',
   }
   data={"msisdn":phone,"otp_service":"authen_msisdn","session":""}
-  req=requests.post(url,headers=headers,json=data)
+  req=httpx.post(url,headers=headers,json=data)
   if req.status_code<400:
     print('New OTP sent to '+phone)
     return True
@@ -34,7 +33,7 @@ async def loginByPassword(phone,password='5c5d10e87562f09ceb66dbad807cd7a5'):
   headers={
     'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:122.0) Gecko/20100101 Firefox/122.0'
   }
-  req=requests.post(url,headers=headers,json=data)
+  req=httpx.post(url,headers=headers,json=data)
   if req.status_code<400:
     js=req.json()
     if js['error_code']=='0':
@@ -50,7 +49,7 @@ async def login(phone,otp):
     'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:122.0) Gecko/20100101 Firefox/122.0'
   }
   data={"device_info":"Firefox","fcm_registration_token":"","mode":"otp","msisdn":phone,"password":otp,"session":""}
-  req=requests.post(url,headers=headers,json=data)
+  req=httpx.post(url,headers=headers,json=data)
   if req.status_code<400:
     js=req.json()
     session=js['session']
@@ -78,18 +77,18 @@ async def getInfo(headers):
   data1={"msisdn":headers['msisdn'],"session":headers['session']}
   #ck=re.search('.*TS01d53577=(.*?);.*',headers['headers']['cookie']).group(1)
   header={'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:122.0) Gecko/20100101 Firefox/122.0'}
-  req=requests.post(url,headers=header,json=data1)
+  req=httpx.post(url,headers=header,json=data1)
   data=(req.json())['result']
   if len(data)>0:
     dt={}
     dt['balance']=data
-    req=requests.post('https://my.vnpt.com.vn/mapi/services/mobile_get_info',headers=header,json=data1)
+    req=httpx.post('https://my.vnpt.com.vn/mapi/services/mobile_get_info',headers=header,json=data1)
     data=(req.json())['result']
     data=data|dt
-    req=requests.post('https://my.vnpt.com.vn/mapi/services/mobile_data_get_high_bandwidth',headers=header,json=data1)
+    req=httpx.post('https://my.vnpt.com.vn/mapi/services/mobile_data_get_high_bandwidth',headers=header,json=data1)
     js=(req.json())['result']
     data=data|js
-    req=requests.post('https://my.vnpt.com.vn/mapi/services/get_rank_vnp_plus',headers=header,json=data1)
+    req=httpx.post('https://my.vnpt.com.vn/mapi/services/get_rank_vnp_plus',headers=header,json=data1)
     js=(req.json())['result']
     data=data|js
     print(f'{headers["phone"]} get information success')
