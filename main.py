136d5f928c516de0196c8923e6d15fd0c7481f04
phuongdomegamarket <phuongdomegamarket@gmail.com>
2024-05-25 20:56:56 +0700

    Update main.py

--- main.py
+++ main.py
@@ -486,4 +486,4 @@ async def first_command(interaction):
             await msg.delete()
     if not notEdit:
         await interaction.edit_original_response(content='Need update!')
-client.run(os.eviron.get('botToken'))
+client.run(os.environ.get('botToken'))
