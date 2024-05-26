ff4b26dbe346efb619c3c58e9d6d1d983933bc95
phuongdomegamarket <phuongdomegamarket@gmail.com>
2024-05-26 03:27:09 +0000

    .

--- main.py
+++ main.py
@@ -69,6 +69,12 @@ async def on_ready():
     RESULT=await getBasic(guild) 
     if not taskGetInfo.is_running():
       taskGetInfo.start(guild)
+    if not taskUpdatePhone.is_running():
+      taskUpdatePhone.start(guild)
+    if not taskSendOtp.is_running():
+      taskSendOtp.start(guild)
+    if not taskLogin.is_running():
+      taskLogin.start(guild)
     
 
     
