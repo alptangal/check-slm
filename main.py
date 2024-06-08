229a6115674e1b75afb940c498aec8dd780fc8b0
alptangal <alptangal@gmail.com>
2024-06-08 18:10:44 +0700

    .

--- main.py
+++ main.py
@@ -543,4 +543,4 @@ async def first_command(interaction):
       await interaction.edit_original_response(content='Danh sách gói cước ưu đãi áp dụng cho thuê bao **'+interaction.channel.name+'**',attachments =[discord.File(fileName)])
   else:
     await interaction.edit_original_response(content='No data')
-client.run('MTIwOTQ4NjcxNDY0MTQ0OTAwMA.GF43Vz.RPWxpVmVzi6YhrECaIPlRRziCo2yT1Bb4H-Tug')#os.environ.get('botToken'))
+client.run(os.environ.get('botToken'))
